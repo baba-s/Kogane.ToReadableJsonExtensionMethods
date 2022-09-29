72ed634c699d15b58302fe716023b18a913086c1
baba-s <shotababa1206@gmail.com>
2022-09-29 19:44:35 +0900

    add

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Kogane.ToReadableJsonExtensionMethods
+# Kogane To Readable Json Extension Methods
+
+整形済みの JSON 形式の文字列に変換して返す拡張メソッド
---
+++ Runtime/ToReadableJsonExtensionMethods.cs
@@ -0,0 +1,13 @@
+﻿namespace Kogane
+{
+    public static class ToReadableJsonExtensionMethods
+    {
+        /// <summary>
+        /// 指定されたインスタンスを JSON に変換して返します
+        /// </summary>
+        public static string ToReadableJson<T>( this T self )
+        {
+            return JsonSerializer.Serialize( self );
+        }
+    }
+}
